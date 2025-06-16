9a476ab8e048a01f44eca2d893c0cac49cc00597
Darkwaslostinthemiddlenight <cytronx7@gmail.com>
2025-06-17 00:51:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def monitor_thread(monitor):
         time.sleep(monitor["interval"])
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host= "0.0.0.0" , debug=True, port=5000)
